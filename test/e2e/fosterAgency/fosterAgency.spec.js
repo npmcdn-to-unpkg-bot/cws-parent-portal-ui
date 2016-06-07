c8a6eade2a13f40a9c88f27cf4a0c2298ac78219
Justin Castillo <jcastillo@emtrain.com>
2016-06-06 23:07:28 -0700

    update tests to take into account that there might not be results for user

--- test/e2e/fosterAgency/fosterAgency.spec.js
+++ test/e2e/fosterAgency/fosterAgency.spec.js
@@ -19,41 +19,67 @@ describe('listing foster agencies', function() {
            }
         });
     });
-/*
-    it('should populate facility_name in the agency listing', function(){
-        browser.get('/#/foster-agency');
 
-        expect(element.all(by.id('foster-agency-name')).get(0).getText()).not.toBe('');
-    });
+    describe('foster agencies are displayed on screen', function(){
+        beforeEach(function () {
+            browser.get('/#/foster-agency');
 
-    it('should populate foster agency facility type in the agency listing', function(){
-        browser.get('/#/foster-agency');
+            agencyList = element.all(by.repeater('fosterAgency in fosterAgencies'));
+        })
 
-        expect(element.all(by.id('foster-agency-facility-type')).get(0).getText()).not.toBe('');
-    });
-
-    it('should populate foster agency phone in the agency listing', function(){
-        browser.get('/#/foster-agency');
-
-        expect(element.all(by.id('foster-agency-phone')).get(0).getText()).not.toBe('');
-    });
-
-    it('should populate foster agency location in the agency listing', function(){
-        browser.get('/#/foster-agency');
-
-        expect(element.all(by.id('foster-agency-location')).get(0).getText()).not.toBe('');
-    });
-
-    it('should return only agencies in the same zipcode', function(){
-        browser.get('/#/foster-agency');
-        zips = element.all(by.id('foster-agency-zip'));
-        allSame = true;
-
-        zips.getText().then(function(value_array) {
-            for(var i = 1; i < value_array.length; i++) {
-                expect(value_array[i] == value_array[0]).toBeTruthy();
-            }
+        it('should populate facility_name in the agency listing', function(){
+            agencyList.count().then(function(count) {
+                if(count != 0) {
+                    expect(element.all(by.id('foster-agency-name')).get(0).getText()).not.toBe('');
+                }
+            });
         });
-    });
-    */
+
+        it('should populate foster agency facility type in the agency listing', function(){
+            agencyList.count().then(function(count) {
+                if(count != 0) {
+                    expect(element.all(by.id('foster-agency-facility-type')).get(0).getText()).not.toBe('');
+                }
+            });
+        });
+
+        it('should populate foster agency phone in the agency listing', function(){
+            agencyList.count().then(function(count) {
+                if(count != 0) {
+                    expect(element.all(by.id('foster-agency-phone')).get(0).getText()).not.toBe('');
+                }
+            });
+        });
+
+        it('should populate foster agency location in the agency listing', function(){
+            agencyList.count().then(function(count) {
+                if(count != 0) {
+                    expect(element.all(by.id('foster-agency-location')).get(0).getText()).not.toBe('');
+                }
+            });
+        });
+
+        it('should populate foster agency location in the agency listing', function(){
+            agencyList.count().then(function(count) {
+                if(count != 0) {
+                    expect(element.all(by.id('foster-agency-location')).get(0).getText()).not.toBe('');
+                }
+            });
+        });
+
+        it('should return only agencies in the same zipcode', function(){
+            zips = element.all(by.id('foster-agency-zip'));
+            allSame = true;
+
+            agencyList.count().then(function(count) {
+                if(count != 0) {
+                    zips.getText().then(function (value_array) {
+                        for (var i = 1; i < value_array.length; i++) {
+                            expect(value_array[i] == value_array[0]).toBeTruthy();
+                        }
+                    });
+                }
+            });
+        });
+    })
 });
