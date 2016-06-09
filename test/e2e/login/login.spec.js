3389b4d8b10d6c3c5eb8e64304ee2184d78559c4
Justin Castillo <jcastillo@emtrain.com>
2016-06-09 03:24:41 -0700

    added tests for create profile and login

--- test/e2e/login/login.spec.js
+++ test/e2e/login/login.spec.js
@@ -1,8 +1,48 @@
-describe("hello-protractor", function () {
-    describe("login", function () {
-        it("should display the correct page url", function () {
-            browser.get('/#');
-            expect(browser.driver.getCurrentUrl()).toBe('http://localhost:3000/#/messages/inbox');
-        });
+describe("login", function () {
+
+
+    beforeEach(function() {
+        browser.get('/#');
+    });
+
+    it('should display the correct page url', function () {
+        expect(browser.getCurrentUrl()).toContain('#/login');
+    });
+
+    it("should allow new users a link to register", function() {
+        expect(element(by.linkText('Register')).getAttribute('href')).toContain('#/create-profile');
+    });
+
+    //Unfortunately cannot test for incorrect login credentials because chromedriver has an issue that will be fixed in 2.22 that
+    // allows the browser to wait for alerts.
+    /*
+    it("should give a message to a user if email/password is not valid", function() {
+        element(by.id('email')).sendKeys('invalid.email@example.com');
+        element(by.id('password')).sendKeys('xyz');
+        element(by.xpath("//input[@value='Log in']")).click();
+
+        var EC = protractor.ExpectedConditions;
+
+        browser.wait(EC.alertIsPresent(), 10000);
+
+        var alertDialog = browser.switchTo().alert();
+
+        expect(alertDialog.getText()).toEqual('Hello');
+
+        alertDialog.accept();
+    });*/
+
+    it('should allow users to login with a proper email/password combination', function() {
+        element(by.id('email')).sendKeys(browser.params.email);
+        element(by.id('password')).sendKeys(browser.params.password);
+        element(by.buttonText('Log in')).click();
+/*
+        return browser.wait(function() {
+            return browser.getCurrentUrl().then(function(url) {
+                return /messages/.test(url);
+            });
+        }, 10000); */
+
+        expect(browser.getCurrentUrl()).toContain('#/messages/inbox');
     });
 });
