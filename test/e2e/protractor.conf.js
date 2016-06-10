97126041a97c6a923849cf7b24e5adace3892817
Justin Castillo <jcastillo@emtrain.com>
2016-06-09 22:03:12 -0700

    more tests

--- test/e2e/protractor.conf.js
+++ test/e2e/protractor.conf.js
@@ -3,7 +3,9 @@ exports.config = {
     specs: [
         'createProfile/createProfile.spec.js',
         'login/login.spec.js',
-        'fosterAgency/fosterAgency.spec.js'
+        'editProfile/editProfile.spec.js',
+        'fosterAgency/fosterAgency.spec.js',
+        'editProfile/editProfile.spec.js'
     ],
 
     baseUrl: 'http://localhost:3000',
